261cb5cc9aa103bce9f5a1214ea1010b2a0a8d98
daniel molina <bigdpower@icloud.com>
2020-06-12 10:54:58 -0700

    initial setups added

---
+++ db/config/models/controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+//   router.post("/api/cats", function(req, res) {
+//     cat.create([
+//       "name", "sleepy"
+//     ], [
+//       req.body.name, req.body.sleepy
+//     ], function(result) {
+      
+//       res.json({ id: result.insertId });
+//     });
+//   });
+  
+//   router.put("/api/cats/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
+  
+//     console.log("condition", condition);
+  
+//     cat.update({
+//       sleepy: req.body.sleepy
+//     }, condition, function(result) {
+//       if (result.changedRows == 0) {
+        
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
+  
+  
+  module.exports = router;
+  
