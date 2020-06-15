ebe8d38ffba71980e1c98292fd9cfa6bea004805
daniel molina <bigdpower@icloud.com>
2020-06-15 15:20:20 -0700

    changed heroku setting for deployment

--- models/burger.js
+++ models/burger.js
@@ -16,23 +16,9 @@ var burger = {
     orm.update("burger", objColsVals, condition, function(res) {
       cb(res);
     });
-  },
+  }
+}
 
 
-reset: function(cb){
-  let vals = false;
-  let cols = "devoured";
-  let condition1 = "id <= 4 ";
-  orm.update("burger", vals, cols, condition1, function(res1){
-    console.log(res1);
-
-  })
-  let condition2 = "id > 4";
-  orm.delete("burger", condition2, function(res){
-    cb(res);
-  });
- },
-
-};
 // Export the database functions for the controller (catsController.js).
 module.exports = burger;
